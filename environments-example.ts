329691547126b223b4a6c50c7f647c6c729886b5
FF-igoicoa <ignacio.goicoa@firstfoundry.co>
2022-07-11 14:48:17 -0300

    Cambio variables de entorno

--- environments-example.ts
+++ environments-example.ts
@@ -1,13 +1,11 @@
 export const environment = {
     production: false,
-    firebaseConfig: {
-      apiKey: "xxx",
-      authDomain: "xxx.firebaseapp.com",
-      databaseURL: "https://xxx.firebaseio.com",
-      projectId: "xxx",
-      storageBucket: "xxx.appspot.com",
-      messagingSenderId: "xxx",
-      appId: "xxx"
-    },
+    firebaseConfig_apiKey: "xxx-YQ4",
+    firebaseConfig_authDomain: "xxx.firebaseapp.com",
+    firebaseConfig_projectId: "https://xxx.firebaseio.com",
+    firebaseConfig_storageBucket: "xxx.appspot.com",
+    firebaseConfig_messagingSenderId: "xxx",
+    firebaseConfig_appId: "xxx",
+    firebaseConfig_measurementId: "xxx",
     admin_wallet_address: "0x0851C7F30B6eE27DC7cbBD9Fc261D0302241FFCf"
  };
