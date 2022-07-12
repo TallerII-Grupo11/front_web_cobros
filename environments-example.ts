3b0adc901f73dbc6da68fb844538562e85d0c732
Tomás Macía <tomasmacia@gmail.com>
2022-07-11 21:29:37 -0300

    Deploy (#4)
    * Deploy
    
    * update dependencies legacy
    
    * update deps
    
    * Web compras fiuba coin (#6)
    
    * Fix web cobros
    
    * Actualizo wallet de admin
    
    Co-authored-by: FF-igoicoa <ignacio.goicoa@firstfoundry.co>
    
    * Remove test run
    
    * missing save in merge
    
    Co-authored-by: Ignacio Goicoa <igoicoa@hotmail.com>
    Co-authored-by: FF-igoicoa <ignacio.goicoa@firstfoundry.co>

--- environments-example.ts
+++ environments-example.ts
@@ -7,5 +7,5 @@ export const ENV = {
     firebaseConfig_messagingSenderId: "xxx",
     firebaseConfig_appId: "xxx",
     firebaseConfig_measurementId: "xxx",
-    admin_wallet_address: "0x2722E3f9BA79904650B88f530fa98B519f3742Bc"
+    admin_wallet_address: "0x3740E06c36d2978c37692FaF12De52F634f63541"
  };
